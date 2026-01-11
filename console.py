3d38d4ad5a0678fbe102ba2dc3aebc1cd508d40c
Teniolaaaa <t.olaleye@alustudent.com>
2026-01-11 14:09:04 +0200

    Task 6: Console 0.0.1 - only quit, EOF, help

--- console.py
+++ console.py
@@ -4,30 +4,12 @@ Console module for AirBnB clone project.
 Contains the entry point of the command interpreter.
 """
 import cmd
-import shlex
-from models import storage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter for AirBnB clone"""
 
     prompt = "(hbnb) "
-    __classes = {
-        'BaseModel': BaseModel,
-        'User': User,
-        'State': State,
-        'City': City,
-        'Amenity': Amenity,
-        'Place': Place,
-        'Review': Review
-    }
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -41,141 +23,6 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing when an empty line is entered"""
         pass
-
-    def do_create(self, arg):
-        """Create a new instance of BaseModel, saves it and prints the id
-        Usage: create <class name>
-        """
-        if not arg:
-            print("** class name missing **")
-            return
-        args = shlex.split(arg)
-        class_name = args[0]
-        if class_name not in self.__classes:
-            print("** class doesn't exist **")
-            return
-        new_instance = self.__classes[class_name]()
-        new_instance.save()
-        print(new_instance.id)
-
-    def do_show(self, arg):
-        """Print string representation of an instance
-        Usage: show <class name> <id>
-        """
-        if not arg:
-            print("** class name missing **")
-            return
-        args = shlex.split(arg)
-        class_name = args[0]
-        if class_name not in self.__classes:
-            print("** class doesn't exist **")
-            return
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-        obj_id = args[1]
-        key = "{}.{}".format(class_name, obj_id)
-        objects = storage.all()
-        if key not in objects:
-            print("** no instance found **")
-            return
-        print(objects[key])
-
-    def do_destroy(self, arg):
-        """Delete an instance based on the class name and id
-        Usage: destroy <class name> <id>
-        """
-        if not arg:
-            print("** class name missing **")
-            return
-        args = shlex.split(arg)
-        class_name = args[0]
-        if class_name not in self.__classes:
-            print("** class doesn't exist **")
-            return
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-        obj_id = args[1]
-        key = "{}.{}".format(class_name, obj_id)
-        objects = storage.all()
-        if key not in objects:
-            print("** no instance found **")
-            return
-        del objects[key]
-        storage.save()
-
-    def do_all(self, arg):
-        """Print all string representation of all instances
-        Usage: all [class name]
-        """
-        args = shlex.split(arg)
-        objects = storage.all()
-        obj_list = []
-        if not arg:
-            for obj in objects.values():
-                obj_list.append(str(obj))
-        else:
-            class_name = args[0]
-            if class_name not in self.__classes:
-                print("** class doesn't exist **")
-                return
-            for key, obj in objects.items():
-                if key.startswith(class_name + "."):
-                    obj_list.append(str(obj))
-        print(obj_list)
-
-    def do_update(self, arg):
-        """Update an instance based on the class name and id
-        Usage: update <class name> <id> <attribute name> "<attribute value>"
-        """
-        if not arg:
-            print("** class name missing **")
-            return
-        args = shlex.split(arg)
-        if len(args) < 1:
-            print("** class name missing **")
-            return
-        class_name = args[0]
-        if class_name not in self.__classes:
-            print("** class doesn't exist **")
-            return
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-        obj_id = args[1]
-        key = "{}.{}".format(class_name, obj_id)
-        objects = storage.all()
-        if key not in objects:
-            print("** no instance found **")
-            return
-        if len(args) < 3:
-            print("** attribute name missing **")
-            return
-        if len(args) < 4:
-            print("** value missing **")
-            return
-        attr_name = args[2]
-        attr_value = args[3]
-        if attr_name in ['id', 'created_at', 'updated_at']:
-            return
-        obj = objects[key]
-        if hasattr(obj, attr_name):
-            attr_type = type(getattr(obj, attr_name))
-            try:
-                attr_value = attr_type(attr_value)
-            except (ValueError, TypeError):
-                pass
-        else:
-            try:
-                attr_value = int(attr_value)
-            except ValueError:
-                try:
-                    attr_value = float(attr_value)
-                except ValueError:
-                    pass
-        setattr(obj, attr_name, attr_value)
-        obj.save()
 
 
 if __name__ == '__main__':
